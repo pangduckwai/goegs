e9f2a5507ac1cf57e13f5da0ef8f1296228a4594
pangduckwai <paul.lai@sea9.org>
2020-11-13 18:13:06 +0800

    Add dynamic printf format test

--- hostos/hostos.go
+++ hostos/hostos.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"runtime"
 )
 
@@ -17,4 +18,13 @@ func main() {
 		// plan9, windows...
 		fmt.Printf("%s.\n", os)
 	}
+
+	v0 := 7
+	v1 := 87
+	v2 := 777
+	format := fmt.Sprintf("Hello [%%%dv]", int(math.Log10(float64(v1)))+1)
+	fmt.Println(format)
+	fmt.Printf(format+" there.\n", v0)
+	fmt.Printf(format+" there?\n", v1)
+	fmt.Printf(format+" there!\n", v2)
 }
