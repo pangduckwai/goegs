be92a8ea10d5de130c268f0fe22cda6bb09dec72
pangduckwai <paul.lai@sea9.org>
2021-04-16 15:58:06 +0800

    Add read sub-tree function

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -4,107 +4,14 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"sea9.org/go/mongo-test/node"
 )
 
-const impl = "MONGOTEST"
+const impl = "BSCTTTCCDMT5XT0MTD-6"
 const monurl = "mongodb://192.168.56.31:27017"
 const mondb = "test"
 const moncoll = "mct"
-
-func buildNodes(head *node.Node) {
-	var next *node.Node
-	curr := head
-	curr.Moves = 42
-	curr.Runs = 16
-	curr.Wins = 3
-
-	// 1st level
-	next = node.New(curr, "", 0, 2, []uint8{10}, 0)
-	next.Moves = 41
-	next.Runs = 2
-	curr.Next = append(curr.Next, next)
-
-	next = node.New(curr, "", 0, 2, []uint8{17}, 0)
-	next.Moves = 41
-	next.Runs = 7
-	next.Wins = 1
-	curr.Next = append(curr.Next, next)
-
-	next = node.New(curr, "", 0, 2, []uint8{34}, 0)
-	next.Moves = 41
-	next.Runs = 2
-	curr.Next = append(curr.Next, next)
-
-	next = node.New(curr, "", 0, 2, []uint8{39}, 0)
-	next.Moves = 41
-	next.Runs = 2
-	curr.Next = append(curr.Next, next)
-
-	next = node.New(curr, "", 0, 2, []uint8{7}, 0)
-	next.Moves = 41
-	next.Runs = 2
-	curr.Next = append(curr.Next, next)
-
-	// 2nd level
-	curr = head.Next[0]
-	next = node.New(curr, "", 1, 2, []uint8{20}, 0)
-	next.Moves = 40
-	next.Runs = 1
-	curr.Next = append(curr.Next, next)
-
-	curr = head.Next[1]
-	next = node.New(curr, "", 1, 2, []uint8{5}, 0)
-	next.Moves = 40
-	next.Runs = 1
-	curr.Next = append(curr.Next, next)
-	next = node.New(curr, "", 1, 2, []uint8{9}, 0)
-	next.Moves = 40
-	next.Runs = 4
-	next.Wins = 1
-	curr.Next = append(curr.Next, next)
-	next = node.New(curr, "", 1, 2, []uint8{8}, 0)
-	next.Moves = 40
-	next.Runs = 1
-	curr.Next = append(curr.Next, next)
-
-	curr = head.Next[2]
-	next = node.New(curr, "", 1, 2, []uint8{4}, 0)
-	next.Moves = 40
-	next.Runs = 1
-	curr.Next = append(curr.Next, next)
-
-	curr = head.Next[3]
-	next = node.New(curr, "", 1, 2, []uint8{29}, 0)
-	next.Moves = 40
-	next.Runs = 1
-	curr.Next = append(curr.Next, next)
-
-	curr = head.Next[4]
-	next = node.New(curr, "", 1, 2, []uint8{21}, 0)
-	next.Moves = 40
-	next.Runs = 1
-	curr.Next = append(curr.Next, next)
-
-	// 3rd level
-	curr = head.Next[1].Next[1]
-	next = node.New(curr, "", 2, 2, []uint8{11}, 0)
-	next.Moves = 39
-	next.Runs = 1
-	next.Wins = 1
-	curr.Next = append(curr.Next, next)
-	next = node.New(curr, "", 2, 2, []uint8{12}, 0)
-	next.Moves = 39
-	next.Runs = 1
-	curr.Next = append(curr.Next, next)
-	next = node.New(curr, "", 2, 2, []uint8{13}, 0)
-	next.Moves = 39
-	next.Runs = 1
-	curr.Next = append(curr.Next, next)
-
-	// 4th level
-	curr = head.Next[1].Next[1].Next[0]
-}
 
 func main() {
 	fmt.Println("[MONGO-TEST]######################################################")
@@ -127,7 +34,22 @@ func main() {
 		log.Fatalf("[Error] %v\n", err)
 	}
 	fmt.Println("[MONGO-TEST] node read:", elapsed)
-	buildNodes(head)
+
+	// Walk thru the entire tree
+	var curr *node.Node
+	queue := []*node.Node{head}
+	for len(queue) > 0 {
+		curr = queue[0]
+
+		if curr.Next == nil && curr.ID != primitive.NilObjectID {
+			_, _, _, err := node.Next(curr)
+			if err != nil {
+				log.Fatalf("[Error] %v\n", err)
+			}
+		}
+
+		queue = append(queue[1:], curr.Next...)
+	}
 
 	if valid, count := node.Validate(head); valid {
 		fmt.Printf("[MONGO-TEST] Validated %v nodes: okay\n", count)
