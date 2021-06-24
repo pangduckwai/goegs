b0138b69451874804bb64cf957e494d593bb16f4
pangduckwai <paul.lai@sea9.org>
2021-06-24 19:07:37 +0800

    Add full stack trace

--- test/test.go
+++ test/test.go
@@ -86,4 +86,12 @@ math/rand.(*Rand).Perm(0xc0002e7170, 0x26, 0xc02ea2c390, 0x26, 0x2a)
         /usr/local/go/src/math/rand/rand.go:225 +0x9c
 sea9.org/go/wdomc/game.Variant1.ChooseMove(0xc03d9a70a0, 0x0, 0x967bc0, 0xc01101af80, 0xc01101b000, 0xc023b7fb0f)
         /home/paul_lai/go/wdom-mc/game/variants.go:137 +0x4de
+sea9.org/go/wdomc/node.Expand(0xc0002e7170, 0xc01101b000, 0x6, 0x1c03, 0x2c, 0xc023b7fc98, 0xa80860, 0xc01101b000, 0xc01101b000, 0x26, ...)
+        /home/paul_lai/go/wdom-mc/node/node.go:154 +0x5b
+sea9.org/go/wdomc/game.Playout(0xc0002d0750, 0x4, 0xc0002d0754, 0x6, 0xc03d9a70a0, 0xc00ba00600, 0xa80878, 0x100000e, 0x23cb000023cc, 0x64, ...)
+        /home/paul_lai/go/wdom-mc/game/game.go:275 +0x5e9
+main._simulate.func1(0xc0002da000, 0xc134f76780, 0xc070a12948, 0x16, 0x1b8, 0xc0002d4e00, 0x70, 0x70, 0xc00ba00600, 0xc134f76840, ...)
+        /home/paul_lai/go/wdom-mc/simulation.go:235 +0x2f3
+created by main._simulate
+        /home/paul_lai/go/wdom-mc/simulation.go:227 +0x270
 */
