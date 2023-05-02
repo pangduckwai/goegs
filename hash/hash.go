c3f5fe044dcfe921b7ba50b9dbbd2c685581d128
pangduckwai <paul.lai@sea9.org>
2023-05-02 17:11:49 +0800

    Update example on psql

--- hash/hash.go
+++ hash/hash.go
@@ -1,43 +1,94 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/binary"
+	"crypto/md5"
+	"encoding"
 	"fmt"
+	"log"
 )
 
-// Node test
-type Node struct {
-	ID     string
-	Move   uint32 // (pkey) indicate progress of the game
-	Turn   uint8  // (pkey) identify the player
-	Action uint8  // (pkey) Actions a player may takes
-	Index1 uint8  // (pkey) indices of players, territories or cards (Depends on the event)
-	Index2 uint8  // (pkey) indices of players, territories or cards (Depends on the event)
-	Index3 uint8  // (pkey) indices of players, territories or cards (Depends on the event)
-	Value1 uint32 // (pkey) Event payload, mainly troop amount, etc (Depends on the event)
-	Value2 uint32 // (pkey) Event payload, mainly troop amount, etc (Depends on the event)
-	Value3 uint32 // (pkey) Event payload, mainly troop amount, etc (Depends on the event)
-	Runs   uint64 // Total number of runs
-	Wins   uint64 // Total number of won games of this player
-	Parent *Node
-	Next   []*Node
-}
+// // Node test
+// type Node struct {
+// 	ID     string
+// 	Move   uint32 // (pkey) indicate progress of the game
+// 	Turn   uint8  // (pkey) identify the player
+// 	Action uint8  // (pkey) Actions a player may takes
+// 	Index1 uint8  // (pkey) indices of players, territories or cards (Depends on the event)
+// 	Index2 uint8  // (pkey) indices of players, territories or cards (Depends on the event)
+// 	Index3 uint8  // (pkey) indices of players, territories or cards (Depends on the event)
+// 	Value1 uint32 // (pkey) Event payload, mainly troop amount, etc (Depends on the event)
+// 	Value2 uint32 // (pkey) Event payload, mainly troop amount, etc (Depends on the event)
+// 	Value3 uint32 // (pkey) Event payload, mainly troop amount, etc (Depends on the event)
+// 	Runs   uint64 // Total number of runs
+// 	Wins   uint64 // Total number of won games of this player
+// 	Parent *Node
+// 	Next   []*Node
+// }
 
-func hash(n *Node) string {
-	a := make([]byte, 4)
-	b := make([]byte, 4)
-	c := make([]byte, 4)
-	d := make([]byte, 4)
-	binary.LittleEndian.PutUint32(a, n.Move)
-	binary.LittleEndian.PutUint32(b, n.Value1)
-	binary.LittleEndian.PutUint32(c, n.Value2)
-	binary.LittleEndian.PutUint32(d, n.Value3)
-	return fmt.Sprintf("%x\n", sha1.Sum([]byte{
-		a[0], a[1], a[2], a[3], n.Turn, n.Action, n.Index1, n.Index2, n.Index3, b[0], b[1], b[2], b[3], c[0], c[1], c[2], c[3], d[0], c[1], c[2], c[3],
-	}))
-}
+// func hash(n *Node) string {
+// 	a := make([]byte, 4)
+// 	b := make([]byte, 4)
+// 	c := make([]byte, 4)
+// 	d := make([]byte, 4)
+// 	binary.LittleEndian.PutUint32(a, n.Move)
+// 	binary.LittleEndian.PutUint32(b, n.Value1)
+// 	binary.LittleEndian.PutUint32(c, n.Value2)
+// 	binary.LittleEndian.PutUint32(d, n.Value3)
+// 	return fmt.Sprintf("%x\n", sha1.Sum([]byte{
+// 		a[0], a[1], a[2], a[3], n.Turn, n.Action, n.Index1, n.Index2, n.Index3, b[0], b[1], b[2], b[3], c[0], c[1], c[2], c[3], d[0], c[1], c[2], c[3],
+// 	}))
+// }
+
+// func conv(v []byte) []uint64 {
+// 	l := len(v)
+// 	r := make([]uint64, 2)
+// 	for k := 0; k < l; k++ {
+// 		r[k/8] = r[k/8] | uint64(v[k])<<(((l-k-1)%8)*8)
+// 	}
+// 	return r
+// }
 
 func main() {
-	fmt.Println(hash(&Node{}))
+	s := "TESTING12345-6" // [1 0 3]
+
+	h := md5.New()
+	h.Write([]byte(s))
+	h.Write([]uint8{1, 0, 3})
+	x := h.Sum(nil)
+	fmt.Printf("0 %x\n", x)
+
+	j := md5.New()
+	j.Write([]byte(s))
+	y := j.Sum(nil)
+	fmt.Printf("1 %x\n", y)
+
+	j.Write([]uint8{1})
+	y = j.Sum(nil)
+	fmt.Printf("2 %x\n", y)
+
+	j.Write([]uint8{0})
+	y = j.Sum(nil)
+	fmt.Printf("3 %x\n", y)
+
+	m, ok := j.(encoding.BinaryMarshaler)
+	if !ok {
+		log.Fatal("Cannot marshal")
+	}
+	state, err := m.MarshalBinary()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("X %x\n", state)
+
+	k := md5.New()
+	u, ok := k.(encoding.BinaryUnmarshaler)
+	if !ok {
+		log.Fatal("Cannot Unmarshal")
+	}
+	if err := u.UnmarshalBinary(state); err != nil {
+		log.Fatal(err)
+	}
+	k.Write([]uint8{3})
+	z := k.Sum(nil)
+	fmt.Printf("4 %x\n", z)
 }
--- psql/psql.go
+++ psql/psql.go
@@ -7,53 +7,118 @@ import (
 	"os"
 	"time"
 
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 /*
 create table test (
-	id serial primary key,
-	descn varchar(100),
-	remarks varchar(150),
-	updated_on timestamp not null
+ id serial primary key,
+ descn varchar(100),
+ remarks varchar(150),
+ updated_on timestamp not null
 );
 
-insert into test (descn, remarks, updated_on) values ('Hello again', 'Hello There!!', current_timestamp);
+CREATE TABLE tests (
+ m INTEGER NOT NULL,
+ n INTEGER NOT NULL,
+ o VARCHAR,
+ t TIMESTAMP NOT NULL,
+ PRIMARY KEY (m, n)
+);
 
-select * from test
+insert into tests (m, n, o, t) values (0, 0, 'A', current_timestamp);
+insert into tests (m, n, o, t) values (0, 1, 'A', current_timestamp);
+insert into tests (m, n, o, t) values (0, 2, 'A', current_timestamp);
 */
-func main() {
-	uid := "wdom"
-	host := "192.168.56.42"
+
+// SQL_TEST_SEL select tests
+const SQL_TEST_SEL = "SELECT n, o, t FROM tests WHERE m=$1"
+
+// SQL_TESTS_UPS upsert tests
+const SQL_TESTS_UPS = "WITH t AS (\n" +
+	" INSERT INTO tests (m, n, o, t)\n" +
+	" VALUES ($1, $2, $3, current_timestamp)\n" +
+	" ON CONFLICT ON CONSTRAINT tests_pkey\n" +
+	" DO UPDATE SET\n" +
+	"  o = tests.o || EXCLUDED.o,\n" +
+	"  t = EXCLUDED.t\n" +
+	"  RETURNING xmax)\n" +
+	" SELECT SUM(CASE WHEN NOT xmax = 0 THEN 1 ELSE 0 END) AS upd FROM t"
+
+func run() int {
+	uid := "paul"
+	host := "localhost"
 	port := 5432
 	dbname := "wdom"
-	url := fmt.Sprintf("host=%v port=%v dbname=%v user=%v", host, port, dbname, uid)
+	// dsn := fmt.Sprintf("host=%v port=%v dbname=%v user=%v pool_min_conns=%v pool_max_conns=%v password=s3crEt", host, port, dbname, uid, 5, 10)
+	dsn := fmt.Sprintf("host=%v port=%v dbname=%v user=%v pool_min_conns=%v pool_max_conns=%v", host, port, dbname, uid, 5, 10)
 
-	fmt.Println("URL: ", url)
+	fmt.Printf("URL: %v\n\n", dsn)
 
-	conn, err := pgx.Connect(context.Background(), url)
+	conn, err := pgxpool.New(context.Background(), dsn)
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		return 1
 	}
-	defer conn.Close(context.Background())
+	defer conn.Close()
 
-	rows, err := conn.Query(context.Background(), "select descn, remarks, updated_on from test where descn=$1", "Hello again")
+	var m, n int32
+	var o string
+	var t time.Time
+	var u int
+
+	// Update
+	err = conn.QueryRow(context.Background(), SQL_TESTS_UPS, 0, 2, ".").Scan(&u)
+	if err != nil {
+		log.Fatalf("Insert to database failed: %v\n", err)
+		return 1
+	}
+	fmt.Printf("Insert to database: %v\n\n", u)
+
+	// Insert
+	err = conn.QueryRow(context.Background(), SQL_TESTS_UPS, 0, 3, "A").Scan(&u)
+	if err != nil {
+		log.Fatalf("Insert to database failed: %v\n", err)
+		return 1
+	}
+	fmt.Printf("Insert to database: %v\n\n", u)
+
+	// Select
+	time.Sleep(1 * time.Second)
+
+	m = 0
+	rows, err := conn.Query(context.Background(), SQL_TEST_SEL, m)
 	if err != nil {
 		log.Fatalf("Unable to query database: %v\n", err)
-		os.Exit(1)
+		return 1
 	}
-
 	count := 0
 	for rows.Next() {
 		count++
-		var dsc, rmk string
-		var tms time.Time
-		err := rows.Scan(&dsc, &rmk, &tms)
+		err := rows.Scan(&n, &o, &t)
 		if err != nil {
 			log.Fatalf("Unable to read row: %v\n", err)
-			os.Exit(1)
+			return 1
 		}
-		fmt.Printf("%3v | %-20v | %-30v | %v \n", count, dsc, rmk, tms)
+		fmt.Printf("%3v> | %5v | %5v | %-36v | %v\n", count, m, n, t, o)
 	}
+	if count == 0 {
+		log.Println("No row selected")
+		return 0
+	}
+
+	// err = conn.QueryRow(context.Background(), "select * from trees where v=$1", "WYZ").Scan(&vrn, &tms)
+	// if err != nil && err != pgx.ErrNoRows {
+	// 	log.Fatalf("Unable to query database: %v\n", err)
+	// 	os.Exit(1)
+	// }
+	// fmt.Printf("%-30v | %v \n", vrn, tms)
+
+	return 0
+}
+
+func main() {
+	x := run()
+	time.Sleep(1 * time.Second)
+	os.Exit(x)
 }
