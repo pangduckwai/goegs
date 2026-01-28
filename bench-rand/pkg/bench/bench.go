d4334ee5a9dba7c0fa39a1347c5cbdc2699c09bc
pangduckwai <paul.lai@sea9.org>
2026-01-28 21:53:48 +0800

    Add benchmark and old lib tests

--- bench-rand/pkg/bench/bench.go
+++ bench-rand/pkg/bench/bench.go
@@ -1,15 +1,18 @@
 package bench
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 // max val 9223372036854775807
 const T1 = 1537228672809129301
 const T2 = 3074457345618258602
-const T3 = 4611686018427387904
+const T3 = 4611686018427387904 // +1
 const T4 = 6148914691236517205
 const T5 = 7686143364045646506
 
-func Dice(rnd *rand.Rand) int64 {
+func Dice(rnd *rand.Rand) int {
 	v := rnd.Int63()
 	if v < T1 {
 		return 0
@@ -26,6 +29,24 @@ func Dice(rnd *rand.Rand) int64 {
 	}
 }
 
-// func Sim() {
-// 	rnd := rand.New(rand.NewSource(time.Now().UnixNano()+1))
-// }
+func Sim(id, run int) (out []int, lps time.Duration) {
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano() + int64(id)))
+	out = []int{0, 0, 0, 0, 0, 0}
+	now := time.Now()
+	for range run {
+		out[Dice(rnd)] += 1
+	}
+	lps = time.Since(now)
+	return
+}
+
+func Bench(id, run int) (out []int, lps time.Duration) {
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano() + int64(id)))
+	out = []int{0, 0, 0, 0, 0, 0}
+	now := time.Now()
+	for range run {
+		out[rnd.Intn(6)] += 1
+	}
+	lps = time.Since(now)
+	return
+}
--- bench-rand/pkg/bench/bench_test.go
+++ bench-rand/pkg/bench/bench_test.go
@@ -7,7 +7,29 @@ import (
 
 func TestDivide(t *testing.T) {
 	val := 6000
-	fmt.Printf("1: %v\n", val>>1) // /2
-	fmt.Printf("2: %v\n", val>>2) // /4
-	fmt.Printf("3: %v\n", val>>3) // /8
+	fmt.Printf("TestDivide() 1: %v\n", val>>1) // /2
+	fmt.Printf("TestDivide() 2: %v\n", val>>2) // /4
+	fmt.Printf("TestDivide() 3: %v\n", val>>3) // /8
+}
+
+func TestRange(t *testing.T) {
+	out := make([]int, 0)
+	for i := range 6 {
+		out = append(out, i)
+	}
+	fmt.Printf("TestRange() %v\n", out)
+}
+
+func TestSim(t *testing.T) {
+	totl := 1000000000
+	bnch, lpsb := Bench(1, totl)
+	stat, lpss := Sim(1, totl)
+	fmt.Printf("TestSim() Bench duration: %v; statistic: %v\n", lpsb, bnch)
+	fmt.Printf("TestSim() S i m duration: %v; statistic: %v\n", lpss, stat)
+	fmt.Printf("  0, %v%%\n", float32(stat[0])/float32(totl)*100)
+	fmt.Printf("  1, %v%%\n", float32(stat[1])/float32(totl)*100)
+	fmt.Printf("  2, %v%%\n", float32(stat[2])/float32(totl)*100)
+	fmt.Printf("  3, %v%%\n", float32(stat[3])/float32(totl)*100)
+	fmt.Printf("  4, %v%%\n", float32(stat[4])/float32(totl)*100)
+	fmt.Printf("  5, %v%%\n", float32(stat[5])/float32(totl)*100)
 }
